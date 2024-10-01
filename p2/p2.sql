728184815e4eb07868760fcc7420581cd0a18a00
Zakir Nuriiev <zakir.nuriiev@innervate.com>
2024-10-01 16:43:30 +0300

    fix p2

---
+++ p2/p2.sql
@@ -0,0 +1,2 @@
+SELECT AVG(price) as average_price, MAX(price) as max_price, MIN(price) as min_price 
+FROM mydb.products;
--- p2/p2_avg_max_min.sql
+++
@@ -1,6 +0,0 @@
-SELECT AVG(price) as average_price FROM mydb.products;
-
-SELECT MAX(price) as max_price FROM mydb.products;
-
-SELECT MIN(price) as min_price FROM mydb.products;
-
